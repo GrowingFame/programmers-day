bf33a1bba54f8f5401e9df8081226eb9cc308df2
rostislav <y4nross@yandex.ru>
2022-09-24 10:58:15 +0300

    my code

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
+
 public class Praktikum {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год:");
         int year = scanner.nextInt();
+
         if (isLeapYear(year)) {
-            System.out.println("12.09." + year);
+            System.out.println("12.09." + year);// здесь нужно вывести результат
         } else {
             System.out.println("13.09." + year);
         }
     }
+
     public static boolean isLeapYear(int year) {
-        if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)){
+        if (year % 4 == 0) {
             return true;
-        }else{
+        } else if (year % 400 == 0) {
+            return true;// здесь нужно определить, является ли переданный год високосным
+        } else {
             return false;
         }
     }
